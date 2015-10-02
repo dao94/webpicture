efca19b965346776e444bb11a87eb502808bad74
dao94 <daotc94@gmail.com>
2015-10-02 22:28:48 +0700

    update

--- backend/config/database.php
+++ backend/config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST', 'facecrm'),
             'database'  => env('DB_DATABASE', 'picture'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_PASSWORD', 'thinhaA@2'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
