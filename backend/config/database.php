b598d2dc6f96cdd04e035befcb4b41e3c2928951
dao94 <daotc94@gmail.com>
2015-10-04 23:27:54 +0700

    update

--- backend/config/database.php
+++ backend/config/database.php
@@ -52,29 +52,29 @@ return [
             'prefix'   => '',
         ],
 
-        // 'mysql' => [
-        //     'driver'    => 'mysql',
-        //     'host'      => env('DB_HOST', 'facecrm.vn'),
-        //     'database'  => env('DB_DATABASE', 'picture'),
-        //     'username'  => env('DB_USERNAME', 'picture'),
-        //     'password'  => env('DB_PASSWORD', 'thinhaA@2'),
-        //     'charset'   => 'utf8',
-        //     'collation' => 'utf8_unicode_ci',
-        //     'prefix'    => '',
-        //     'strict'    => false,
-        // ],
-
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
+            'host'      => env('DB_HOST', 'facecrm.vn'),
             'database'  => env('DB_DATABASE', 'picture'),
-            'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', ''),
+            'username'  => env('DB_USERNAME', 'picture'),
+            'password'  => env('DB_PASSWORD', 'thinhaA@2'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
         ],
+
+        // 'mysql' => [
+        //     'driver'    => 'mysql',
+        //     'host'      => env('DB_HOST', 'localhost'),
+        //     'database'  => env('DB_DATABASE', 'picture'),
+        //     'username'  => env('DB_USERNAME', 'root'),
+        //     'password'  => env('DB_PASSWORD', ''),
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        //     'strict'    => false,
+        // ],
 
         'pgsql' => [
             'driver'   => 'pgsql',
