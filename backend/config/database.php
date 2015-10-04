ba05c28121a8dd5015076b46ee18d6242f548ce7
dao94 <daotc94@gmail.com>
2015-10-04 16:11:36 +0700

    update

--- backend/config/database.php
+++ backend/config/database.php
@@ -52,12 +52,24 @@ return [
             'prefix'   => '',
         ],
 
+        // 'mysql' => [
+        //     'driver'    => 'mysql',
+        //     'host'      => env('DB_HOST', 'facecrm.vn'),
+        //     'database'  => env('DB_DATABASE', 'picture'),
+        //     'username'  => env('DB_USERNAME', 'picture'),
+        //     'password'  => env('DB_PASSWORD', 'thinhaA@2'),
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        //     'strict'    => false,
+        // ],
+
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'facecrm.vn'),
+            'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'picture'),
-            'username'  => env('DB_USERNAME', 'picture'),
-            'password'  => env('DB_PASSWORD', 'thinhaA@2'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
