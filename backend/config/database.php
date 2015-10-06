56dcd758bdcfe7660e5731ed4d5868641c64720d
wayc0de <thinhit@yahoo.com>
2015-10-07 01:03:43 +0700

    merge

--- backend/config/database.php
+++ backend/config/database.php
@@ -54,10 +54,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'facecrm.vn'),
+            'host'      => env('DB_HOST', '192.168.1.136'),
             'database'  => env('DB_DATABASE', 'picture'),
-            'username'  => env('DB_USERNAME', 'picture'),
-            'password'  => env('DB_PASSWORD', 'thinhaA@2'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
