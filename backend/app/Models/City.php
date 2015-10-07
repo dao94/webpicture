f75d3ebc588be9ae9a60e1f64bd4f1e46f9784c5
dao94 <daotc94@gmail.com>
2015-10-08 02:18:17 +0700

    update

---
+++ backend/app/Models/City.php
@@ -0,0 +1,11 @@
+<?php namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Config;
+
+class CityModel extends Model {
+
+	protected $table      = 'collections';
+	public $timestamps 	  = false;
+}
